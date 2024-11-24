8fd99f3e207487787d25c27f240d68692f4c8c54
Quang Nguyen <47670944+BinGeek139@users.noreply.github.com>
2024-11-25 00:58:01 +0700

    Update Regex.java

--- Regex.java
+++ Regex.java
@@ -79,4 +79,5 @@ public class UserAgentParser {
 
         return "Unknown";
     }
+    Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36
 }
